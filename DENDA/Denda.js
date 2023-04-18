704d578f7f1c445b2b320b938e51db8b8c20fbe3
anderibanez123 <tsbenpresa@gmail.com>
2023-04-18 08:25:57 +0200

    a

--- DENDA/Denda.js
+++ DENDA/Denda.js
@@ -1,58 +1,3 @@
-//Menuaren Script-a
-const body = document.querySelector('body'),
-sidebar = body.querySelector('nav'),
-toggle = body.querySelector(".toggle"),
-searchBtn = body.querySelector(".search-box"),
-modeSwitch = body.querySelector(".toggle-switch"),
-modeText = body.querySelector(".mode-text");
-
-
-toggle.addEventListener("click" , () =>{
-    sidebar.classList.toggle("close");
-    
-})
-
-searchBtn.addEventListener("click" , () =>{
-    sidebar.classList.remove("close");
-})
-
-modeSwitch.addEventListener("click" , () =>{
-    body.classList.toggle("dark");
-    
-    if(body.classList.contains("dark")){
-        modeText.innerText = "Modu argia";
-    }else{
-        modeText.innerText = "Modu iluna";
-        
-    }
-});
-
-function erabiltzaileaKargatu(){
-  
-  // Obtén el valor almacenado en localStorage
-  var izenaBistaratu = localStorage.getItem("izenaGorde");
-  
-  if(izenaBistaratu == null){
-
-    izenaBistaratu = "Erabiltzailea";
-
-  }
-
-  document.getElementById("izenaBistaratu").textContent = izenaBistaratu;
-
-}
-
-function erabiltzaileIrten() {
-
-  localStorage.clear();
-
-  izenaBistaratu = "Erabiltzailea";
-
-  document.getElementById("izenaBistaratu").textContent = izenaBistaratu;
-
-  location.reload();
-  
-}
 
 /* DENDA BARRUKO GALERIA || CAROUSEL */
 function App() {}
@@ -90,4 +35,62 @@ let nextAction = (leftPosition,trackWidth,listWidth,slickWidth,track) => {
     if(leftPosition < (trackWidth - listWidth)) {
         track.style.left = `${-1 * (leftPosition + slickWidth)}px`;
     }
+}
+
+//Menuaren Script-a
+const body = document.querySelector('body'),
+sidebar = body.querySelector('nav'),
+toggle = body.querySelector(".toggle"),
+searchBtn = body.querySelector(".search-box"),
+modeSwitch = body.querySelector(".toggle-switch"),
+modeText = body.querySelector(".mode-text");
+
+
+toggle.addEventListener("click" , () =>{
+    sidebar.classList.toggle("close");
+    
+})
+
+searchBtn.addEventListener("click" , () =>{
+    sidebar.classList.remove("close");
+})
+
+modeSwitch.addEventListener("click" , () =>{
+    body.classList.toggle("dark");
+    
+    if(body.classList.contains("dark")){
+        modeText.innerText = "Modu argia";
+    }else{
+        modeText.innerText = "Modu iluna";
+        
+    }
+});
+
+function erabiltzaileaKargatu(){
+  
+
+  // Obtén el valor almacenado en localStorage
+  var izenaBistaratu = localStorage.getItem("izenaGorde");
+  
+  if(izenaBistaratu == null){
+
+    izenaBistaratu = "Erabiltzailea";
+
+  }
+
+  document.getElementById("izenaBistaratu").textContent = izenaBistaratu;
+
+}
+
+
+function erabiltzaileIrten() {
+
+  localStorage.clear();
+
+  izenaBistaratu = "Erabiltzailea";
+
+  document.getElementById("izenaBistaratu").textContent = izenaBistaratu;
+
+  location.reload();
+  
 }
